NVIDIAGameWorks/donut

commit 260681fe792d0c7986456abba819fe4b759b128f

Added ability to (optionally) set per-pixel TAA clampingFactor and newFrameWeight. Useful for pixels where we know denoiser can't do a good job (i.e. guide buffers are noisy or curvature too high & random).

src/render/TemporalAntiAliasingPass.cpp
@@ -173,6 +173,10 @@ TemporalAntiAliasingPass::TemporalAntiAliasingPass(
             nvrhi::BindingSetItem::Texture_UAV(0, params.resolvedColor),
             nvrhi::BindingSetItem::Texture_UAV(1, params.feedback2)
         };
+        
+        m_HasHistoryClampRelaxTexture = params.historyClampRelax != nullptr;
+        if (params.historyClampRelax != nullptr)
+            bindingSetDesc.bindings.push_back(nvrhi::BindingSetItem::Texture_SRV(3, params.historyClampRelax));
 
         nvrhi::utils::CreateBindingSetAndLayout(device, nvrhi::ShaderType::Compute, 0, bindingSetDesc, m_ResolveBindingLayout, m_ResolveBindingSet);
      
@@ -268,6 +272,7 @@ void TemporalAntiAliasingPass::TemporalResolve(
         taaConstants.newFrameWeight = feedbackIsValid ? params.newFrameWeight : 1.f;
         taaConstants.pqC = dm::clamp(params.maxRadiance, 1e-4f, 1e8f);
         taaConstants.invPqC = 1.f / taaConstants.pqC;
+        taaConstants.useHistoryClampRelax = (params.useHistoryClampRelax && m_HasHistoryClampRelaxTexture) ? 1 : 0;
         commandList->writeBuffer(m_TemporalAntiAliasingCB, &taaConstants, sizeof(taaConstants));
 
         int2 viewportSize = int2(taaConstants.outputViewSize);

include/donut/render/TemporalAntiAliasingPass.h
@@ -52,6 +52,11 @@ namespace donut::render
         float clampingFactor = 1.0f;
         float maxRadiance = 10000.f;
         bool enableHistoryClamping = true;
+
+        // Requires CreateParameters::historyClampRelax single channel [0, 1] mask to be provided. 
+        // For texels with mask value of 0 the behavior is unchanged; for texels with mask value > 0, 
+        // 'newFrameWeight' will be reduced and 'clampingFactor' will be increased proportionally. 
+        bool useHistoryClampRelax = false;
     };
 
     class TemporalAntiAliasingPass
@@ -81,6 +86,8 @@ namespace donut::render
         dm::float2 m_R2Jitter;
         TemporalAntiAliasingJitter m_Jitter;
 
+        bool m_HasHistoryClampRelaxTexture;
+
     public:
         struct CreateParameters
         {
@@ -90,6 +97,7 @@ namespace donut::render
             nvrhi::ITexture* resolvedColor = nullptr;
             nvrhi::ITexture* feedback1 = nullptr;
             nvrhi::ITexture* feedback2 = nullptr;
+            nvrhi::ITexture* historyClampRelax = nullptr;
             bool useCatmullRomFilter = true;
             uint32_t motionVectorStencilMask = 0;
             uint32_t numConstantBufferVersions = 16;

include/donut/shaders/taa_cb.h
@@ -45,6 +45,9 @@ struct TemporalAntiAliasingConstants
     float invPqC;
 
     uint stencilMask;
+    uint useHistoryClampRelax;
+    uint padding0;
+    uint padding1;
 };
 
 #endif // TAA_CB_H

shaders/passes/taa_cs.hlsl
@@ -45,6 +45,9 @@ SamplerState s_Sampler : register(s0);
 RWTexture2D<float4> u_ColorOutput : register(u0);
 RWTexture2D<float4> u_FeedbackOutput : register(u1);
 
+Texture2D<float> t_HistoryClampRelaxMask : register(t3);
+
+
 #define GROUP_X 16
 #define GROUP_Y 16
 #define BUFFER_X (GROUP_X + 3)
@@ -228,12 +231,31 @@ void main(
         }
     } 
 
+    float clampingFactor = g_TemporalAA.clampingFactor;
+    float newFrameWeight = g_TemporalAA.newFrameWeight;
+    float historyClampRelax = 0;
+    if (g_TemporalAA.useHistoryClampRelax)
+    {
+        // Simple 4-tap 16ish-pixel blur; historyClampRelax is 0 where there's no 'relax' and 1 when there's
+        // 'full relax'; for no blur just use 'historyClampRelax = t_HistoryClampRelaxMask[outputPixelPosition];'
+        float2 centerUV = (float2(outputPixelPosition.xy) + 0.5) * g_TemporalAA.outputTextureSizeInv;
+        float2 offset = g_TemporalAA.outputTextureSizeInv * 0.4; // hand tuned filter width for best effect
+        historyClampRelax =saturate((t_HistoryClampRelaxMask.SampleLevel(s_Sampler, centerUV + offset * float2(-1,-1), 0)
+                           +t_HistoryClampRelaxMask.SampleLevel(s_Sampler, centerUV + offset * float2(+1,-1), 0)
+                           +t_HistoryClampRelaxMask.SampleLevel(s_Sampler, centerUV + offset * float2(-1,+1), 0)
+                           +t_HistoryClampRelaxMask.SampleLevel(s_Sampler, centerUV + offset * float2(+1,+1), 0)) * 0.4); // hand tuned multiplier
+
+        // Slow convergence speed to TAA local factors (hand tuned heuristics)
+        clampingFactor *= (1.0+historyClampRelax*1.5);  // extend the clamp range
+        newFrameWeight /= (1.0+historyClampRelax*0.5);  // reduce the new sample weight
+    }
+
     float2 longestMV = s_MotionVectors[longestMVPos.y][longestMVPos.x];
 
     colorMoment1 /= 9.0;
     colorMoment2 /= 9.0;
     float3 colorVariance = colorMoment2 - colorMoment1 * colorMoment1;
-    float3 colorSigma = sqrt(max(0, colorVariance)) * g_TemporalAA.clampingFactor;
+    float3 colorSigma = sqrt(max(0, colorVariance)) * clampingFactor;
     float3 colorMin = colorMoment1 - colorSigma;
     float3 colorMax = colorMoment1 + colorSigma;
 
@@ -243,7 +265,7 @@ void main(
     float2 sourcePos = float2(outputPixelPosition.xy) + longestMV + 0.5;
 
     float3 resultPQ;
-    if (g_TemporalAA.newFrameWeight < 1.0 && all(sourcePos.xy > g_TemporalAA.outputViewOrigin) 
+    if (newFrameWeight < 1.0 && all(sourcePos.xy > g_TemporalAA.outputViewOrigin) 
         && all(sourcePos.xy < g_TemporalAA.outputViewOrigin + g_TemporalAA.outputViewSize))
     {
 #if USE_CATMULL_ROM_FILTER
@@ -255,7 +277,7 @@ void main(
         // Clamp the old color to the new color distribution
 
         float3 historyClamped = history;
-        if (g_TemporalAA.clampingFactor >= 0)
+        if (clampingFactor >= 0)
         {
             historyClamped = min(colorMax, max(colorMin, history));
         }
@@ -266,7 +288,7 @@ void main(
         float2 distanceToLowResPixel = inputPos - float2(inputPosInt);
         float upscalingFactor = g_TemporalAA.outputOverInputViewSize.x;
         float sampleWeight = saturate(1.0 - upscalingFactor * dot(distanceToLowResPixel, distanceToLowResPixel));
-        float blendWeight = saturate(max(motionWeight, sampleWeight) * g_TemporalAA.newFrameWeight);
+        float blendWeight = saturate(max(motionWeight, sampleWeight) * newFrameWeight);
 
         resultPQ = lerp(historyClamped, thisPixelColor, blendWeight);
     }

commit 0ac7e916ecd0a89f7cf0cae51afe81215cb48b8d

Fixed a validation error introduced in 260681fe792d0c7986456abba819fe4b759b128f

src/render/TemporalAntiAliasingPass.cpp
@@ -176,7 +176,14 @@ TemporalAntiAliasingPass::TemporalAntiAliasingPass(
         
         m_HasHistoryClampRelaxTexture = params.historyClampRelax != nullptr;
         if (params.historyClampRelax != nullptr)
+        {
             bindingSetDesc.bindings.push_back(nvrhi::BindingSetItem::Texture_SRV(3, params.historyClampRelax));
+        }
+        else
+        {
+            // No relax mask, but we need to bind something to match the shader binding slots
+            bindingSetDesc.bindings.push_back(nvrhi::BindingSetItem::Texture_SRV(3, params.unresolvedColor));
+        }
 
         nvrhi::utils::CreateBindingSetAndLayout(device, nvrhi::ShaderType::Compute, 0, bindingSetDesc, m_ResolveBindingLayout, m_ResolveBindingSet);
      